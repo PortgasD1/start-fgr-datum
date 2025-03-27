7f2b9e710de4a1e87dcc4f2c4cd501d1c6fd8ea8
DatumRedsoft <datumredsoft@localhost.localdomain>
2025-03-27 16:24:56 -0500

    Start app with Quarkus

--- start-code-quarkus/src/test/java/com/datum/fgr/QuarkuStartIT.java
+++ start-code-quarkus/src/test/java/com/datum/fgr/QuarkuStartIT.java
@@ -3,6 +3,6 @@ package com.datum.fgr;
 import io.quarkus.test.junit.QuarkusIntegrationTest;
 
 @QuarkusIntegrationTest
-class GreetingResourceIT extends GreetingResourceTest {
+class QuarkuStartIT extends QuarkuStartTest {
     // Execute the same tests but in packaged mode.
 }
